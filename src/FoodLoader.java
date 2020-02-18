fee374ede229b2a7968cf204fd215324f996f603
ZuhaoFang <knoxfang@outlook.com>
2020-02-17 22:14:06 -0600

    Post Gived file to the cloud

---
+++ src/FoodLoader.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class FoodLoader{
+
+	private static final int maxFileLines = 100;
+	
+	// Bronze
+	// Load the String of Food data and return a fully initialized Food object (including ingredients)
+	public static Food parseString(String foodAsString){
+
+		Food foodItem = null;
+
+		return foodItem; // change this to return the initialized object
+	}
+
+	// given a specified file, load it and parse each line into a Food[] type object. Be sure to handle
+	// any exceptions. 
+	public static Food[] loadFile(String fileName){
+
+		// Return data
+		Food[] foodList = new Food[maxFileLines];
+		int count =0;
+
+		return foodList; // return an initialized food array. 
+	}
+
+}
